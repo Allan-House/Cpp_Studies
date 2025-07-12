9906c8c19990271fa95095751d79507419318ed6
Allan-House <allanronaldo313@gmail.com>
2025-07-11 22:34:18 -0300

    Initial commit

---
+++ Character/src/Character.cpp
@@ -0,0 +1,9 @@
+#include "Character.h"
+
+Character::Character() {}
+Character::~Character() {}
+
+void Character::Attack() {}
+bool Character::IsDead() {}
+void Character::Talk(const std::string& speech) {}
+void Character::Display() {}
